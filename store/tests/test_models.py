bf8bc553f10aab5dad7be0cdf6cd91b3944b3693
khot-abhishek <abhishekrkhot@gmail.com>
2022-10-04 22:11:26 +0530

    some Basic test for models basic operations

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,5 +1,7 @@
+from unicodedata import category
 from django.test import TestCase
 from store.models import Product,Category
+from django.contrib.auth.models import User
 
 
 class TestCategoriesModel(TestCase):
@@ -13,3 +15,20 @@ class TestCategoriesModel(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+        self.assertEqual(str(data), 'django')
+        
+
+class TestproductModel(TestCase):
+    
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin', password='Testuser@123')
+        self.data1 = Product.objects.create(category_id=1,created_by_id=1,title='django begineers',slug='django begineers', price='20.99', image='image')
+        
+    def test_product_model_entry(self):
+        """
+        Test product model data insertion/type/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django begineers')
