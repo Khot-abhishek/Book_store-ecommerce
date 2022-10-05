7646227969730df9a2389735e2f753564710556f
khot-abhishek <abhishekrkhot@gmail.com>
2022-10-05 20:42:08 +0530

    added a simple template for main page and its associative views function, and added a custom context to context processor

--- store/views.py
+++ store/views.py
@@ -3,6 +3,9 @@ from .models import Product, Category
 
 # Create your views here.
 
+def categories(request):
+    return {'categories':Category.objects.all()}
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products':products})
